f2de0bd69a163ea3a6564078cd7baf21dd550db0
har1nagle <hnagle.0@yahoo.com>
2024-08-09 11:44:41 +0530

    add step counter

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,25 @@ function Counter() {
   return (
     <div>
       <div>
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
       <p>
-        <span> {count === 0
-          ? "Today is"
-          : count > 0
-          ? `${count} days from today is`
-          : `${Math.abs(count)} days ago was`}
+        <span>
+          {" "}
+          {count === 0
+            ? "Today is"
+            : count > 0
+            ? `${count} days from today is`
+            : `${Math.abs(count)} days ago was`}
         </span>
 
         <span> {date.toDateString()} </span>
